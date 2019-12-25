d63b6388fca87dcffdadaa5b2456ac418ea2d346
Vladimir Feinberg <vladf@berkeley.edu>
2019-12-24 23:08:36 -0800

    include modules

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name="mw", author="Vladimir Feinberg")
+setup(name="mw", author="Vladimir Feinberg", packages=find_packages())
